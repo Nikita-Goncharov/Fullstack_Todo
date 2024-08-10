217cf0608809825d425a0f5868ac6e428fd60c02
Nikita Goncharov <nikitagoncarov657@gmail.com>
2024-08-10 19:55:38 +0300

    add logout component & basic header setup

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,17 @@
+import styles from './Header.module.scss'
+import {Logout} from "../Authorization/Logout/Logout.jsx";
+
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        {/*<Link to="/index">*/}
+        <img className={styles.logoImg} src="/logo.png" alt="Site logo"/>
+        {/*</Link>*/}
+      </div>
+      <div className={styles.logout}>
+        <Logout/>
+      </div>
     </div>
   );
 };
